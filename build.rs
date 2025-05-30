3176ba8d87bbf5d6467ed5cf7d68fc1524dac1e6
LVRodrigues <luciano@codesolver.com.br>
2025-05-30 01:46:53 +0000

    Refactor cbindgen setup in build.rs; remove cbindgen.toml configuration

--- build.rs
+++ build.rs
@@ -1,15 +1,17 @@
 extern crate cbindgen;
 
 use std::env;
+use std::path::Path;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let header_path = Path::new(&crate_dir).join("target").join("rust-interop.h");
 
-    // cbindgen::Builder::new()
-    //     .with_crate(crate_dir)
-    //     .generate()
-    //     .expect("Unable to generate bindings")
-    //     .write_to_file("bindings.h");
-    
-    cbindgen::generate(&crate_dir).expect("Unable to generate bindings");
+    cbindgen::Builder::new()
+        .with_crate(crate_dir)
+        .with_language(cbindgen::Language::Cxx)
+        .with_include_guard("RUST_INTEROP_H")
+        .generate()
+        .expect("Unable to generate bindings")
+        .write_to_file(header_path);
 }
