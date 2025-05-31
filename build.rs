215e4aa95fa0931a5e0cf58a48e21218847a53f2
LVRodrigues <luciano@codesolver.com.br>
2025-05-31 13:13:14 +0000

    Update Cargo.toml and build.rs for improved library configuration and binding generation

--- build.rs
+++ build.rs
@@ -5,13 +5,23 @@ use std::path::Path;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let header_path = Path::new(&crate_dir).join("target").join("rust-interop.h");
+    let header_path = Path::new(&crate_dir)
+        .join("target")
+        .join("include")
+        .join("rust-interop.h");
 
     cbindgen::Builder::new()
         .with_crate(crate_dir)
         .with_language(cbindgen::Language::Cxx)
         .with_include_guard("RUST_INTEROP_H")
         .generate()
-        .expect("Unable to generate bindings")
-        .write_to_file(header_path);
+        .map_or_else(
+            |error| match error {
+                cbindgen::Error::ParseSyntaxError { .. } => {}
+                e => panic!("{:?}", e),
+            },
+            |bindings| {
+                bindings.write_to_file(header_path);
+            },
+        );
 }
