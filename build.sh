28551635d795f33ded54058dda3fb8de7517ccfc
LVRodrigues <luciano@codesolver.com.br>
2025-06-09 17:05:44 -0300

    Reiniciando testes

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cargo build 
-
-g++ -std=c++23 -I target/include  src/main.cpp -L target/debug -l rust_interop -o target/debug/app
-
-export LD_LIBRARY_PATH=target/debug
-target/debug/app
