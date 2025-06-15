c940ca8b83d4e13538c68e57958eab774f95254d
LVRodrigues <luciano@codesolver.com.br>
2025-06-15 21:53:36 +0000

    Versio Info exposed.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <cstddef>
 
+#include "csdemo.h"
+
 int main(int, char**){
-    std::cout << "Hello, from csdemo!\n";
+    VersionInfo version = csdemo_version();
+
+    std::cout << "Library.....: " << version.name << std::endl << 
+                 "Version.....: " << (int) version.major << "." << (int) version.minor << "." << (int) version.patch << std::endl <<
+                 "Description.: " << version.description << std::endl <<
+                 "Author......: " << version.author << std::endl <<
+                 "System......: " << version.system << std::endl <<
+                 "Architecture: " << version.architecture << std::endl;
 
     return EXIT_SUCCESS;
 }
